72acee8d38a4ee88e94f07e0677582d5c4f56189
MARUGO-s <pingus0428@gmail.com>
2025-10-09 06:32:33 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ const groq = new Groq({
 });
 
 const app = express();
+
+// CORS設定
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
